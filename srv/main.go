560deea7a5cbbdd5de910fec8ae13cf74855236f
fluffy <fluffy@fluffysnowman.dev>
2025-02-18 23:04:17 +0530

    update srv/main.go to have actual html at /

--- srv/main.go
+++ srv/main.go
@@ -4,11 +4,21 @@ import (
 	"net/http"
 )
 
+const WEBSITE_CONTENT = `
+<html>
+    <head>
+    </head>
+    <body>
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">ur gay</a>
+    </body>
+</html>
+`
+
 func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello there"))
+		w.Write([]byte(WEBSITE_CONTENT))
 	})
 
   http.ListenAndServe("10.69.42.16:8000", mux);
